43f74f0c52320ec26e3f70eb66d28c9a0f82e708
jay-so <merry9504@naver.com>
2024-05-24 11:49:20 +0900

    feat: JS 데이터 구조(단일 연결 리스트) 학습

---
+++ code/singlyLinkedList/Singly_Linked_List_02.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // traverse(){
+  //   var current = this.head;
+  //   while (current){
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
+
+var list = new SinglyLinkedList();
+list.push("Hello");
+list.push("GoodBye");
+list.push("!");
+
+//Bad
+// var first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
---
+++ code/singlyLinkedList/Singly_Linked_List_06.js
@@ -0,0 +1,114 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // traverse(){
+  //   var current = this.head;
+  //   while (current){
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    var count = 0;
+    var current = this.head;
+    while (counter !== index){
+      current = current.next;
+      current ++;
+    }
+    return current;
+  }
+  set(index,val){
+    var foundNode = this.get(index);
+    if(foundNode){
+      foundNode.val = val;
+      return true;
+    }
+    return  false;
+  }
+}
+
+var list = new SinglyLinkedList();
+list.push("Hello");
+list.push("GoodBye");
+list.push("!");
+list.push("<3");
+list.push(":)");
+
+//Bad
+// var first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
